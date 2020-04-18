e78a46a56bbc6148111b117e9bdb001c185d0b0b
Kester Riley <kesterriley@hotmail.com>
2020-04-18 17:34:42 +0100

    modification of circle ci config

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -73,11 +73,11 @@ package_chart() {
 }
 
 release_charts() {
-    cr upload -o $GIT_USERNAME -r $GIT_REPO 
+    cr upload -o "$GIT_USERNAME" -r "$GIT_REPO" 
 }
 
 update_index() {
-    cr index -o $GIT_USERNAME -r $GIT_REPO -c $GIT_REPOSITORY_URL 
+    cr index -o "$GIT_USERNAME" -r "$GIT_REPO" -c "$GIT_REPOSITORY_URL" 
 
     git config user.email "$GIT_EMAIL"
     git config user.name "$GIT_USERNAME"
