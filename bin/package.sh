b4b44e02638ecd1b797a251e1a6b0e3fdb00e737
Kester Riley <kesterriley@hotmail.com>
2020-05-26 20:42:31 +0100

    ff

--- bin/package.sh
+++ bin/package.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-[[ -f /tmp/charts ]] && mkdir -p /tmp/charts
+mkdir -p /tmp/charts
+
+cd /tmp/charts
+
+git clone https://github.com/kesterriley/my-helm-charts.git
+cd /tmp/charts/my-helm-charts
 
 for chart in charts/*
 do
-  echo $chart
-  if [ $chart == 'charts/README.md' ]
-  then
-     continue
-  fi
-  printf "\nChecking %s\n" "${chart#*/}"
-  helm package ${chart} --destination /tmp/charts
+ if [ $chart == 'charts/README.md' ]
+ then
+    continue
+ fi
+ printf "\nChecking %s\n" "${chart#*/}"
+ helm package ${chart} --destination .
 done
 
 git checkout gh-pages
-#mv /tmp/charts/*.tgz .
-#helm repo index . --url https://kesterriley.github.io/my-helm-charts
-#git add .
-#git commit -m "Publish charts"
-#git push origin gh-pages
-ls -lar /tmp/charts
+helm repo index . --url https://kesterriley.github.io/my-helm-charts
+git add .
+git commit -m "Publish charts"
+git push origin gh-pages
