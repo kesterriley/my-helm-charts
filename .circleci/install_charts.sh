c60bf019ddd7d99ad67e0a161baac7bd55f532d2
Kester Riley <kesterriley@hotmail.com>
2020-05-26 14:36:48 +0100

    Fixed version

--- .circleci/install_charts.sh
+++ .circleci/install_charts.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 readonly CT_VERSION=v2.3.3
-readonly KIND_VERSION=0.8.1
+readonly KIND_VERSION=0.2.1
 readonly CLUSTER_NAME=chart-testing
 readonly K8S_VERSION=v1.14.0
 
