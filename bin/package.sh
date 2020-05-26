ef7002ebc60ea563529a49b6e9d0b9918aa85381
Kester Riley <kesterriley@hotmail.com>
2020-05-26 21:03:49 +0100

    dd

--- bin/package.sh
+++ bin/package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git config --global user.email ${CIRCLE_PROJECT_USERNAME}@users.noreply.github.com
-git config --global user.name "$CIRCLE_PROJECT_USERNAME"
+git config --global user.name "${CIRCLE_PROJECT_USERNAME}"
 
 mkdir -p /tmp/charts
 cd /tmp/charts
@@ -20,7 +20,7 @@ do
 done
 
 git checkout gh-pages
-helm repo index . --url https://${GIT_USERNAME}.github.io/${CIRCLE_PROJECT_REPONAME}
+helm repo index . --url https://${CIRCLE_PROJECT_USERNAME}.github.io/${CIRCLE_PROJECT_REPONAME}
 git add .
 git commit -m "Publish charts"
 git push origin gh-pages
