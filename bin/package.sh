0c0212f40835ca987db93d41b1833515d9072c93
Kester Riley <kesterriley@hotmail.com>
2020-05-26 21:01:13 +0100

    dd

--- bin/package.sh
+++ bin/package.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-git config --global user.email kesterriley@users.noreply.github.com
-git config --global user.name "Kester Riley"
+git config --global user.email ${CIRCLE_PROJECT_USERNAME}@users.noreply.github.com
+git config --global user.name "$CIRCLE_PROJECT_USERNAME"
 
 mkdir -p /tmp/charts
 cd /tmp/charts
 
-git clone https://github.com/kesterriley/my-helm-charts.git
+git clone ${REPOSITORY_URL}
 cd /tmp/charts/my-helm-charts
 
 for chart in charts/*
@@ -20,7 +20,7 @@ do
 done
 
 git checkout gh-pages
-helm repo index . --url https://kesterriley.github.io/my-helm-charts
+helm repo index . --url https://${GIT_USERNAME}.github.io/${CIRCLE_PROJECT_REPONAME}
 git add .
 git commit -m "Publish charts"
 git push origin gh-pages
