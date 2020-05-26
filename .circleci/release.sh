388edab4c82557b05bd6c3dd3158448b1a33325a
Kester Riley <kesterriley@hotmail.com>
2020-05-26 12:50:16 +0100

    Modifications to CircleCI build

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -4,16 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-: "${CR_TOKEN:?Environment variable CR_TOKEN must be set}"
-: "${GIT_REPOSITORY_URL:?Environment variable GIT_REPOSITORY_URL must be set}"
+: "${CH_TOKEN:?Environment variable CH_TOKEN must be set}"
+: "${GIT_REPOSITORY_URL:?Environment variable GIT_REPO_URL must be set}"
 : "${GIT_USERNAME:?Environment variable GIT_USERNAME must be set}"
 : "${GIT_EMAIL:?Environment variable GIT_EMAIL must be set}"
-: "${GIT_REPO:?Environment variable GIT_REPO must be set}"
+: "${GIT_REPOSITORY_NAME:?Environment variable GIT_REPOSITORY_NAME must be set}"
 
 readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 
 main() {
     pushd "$REPO_ROOT" > /dev/null
+
     echo "Fetching tags..."
     git fetch --tags
 
@@ -33,11 +34,8 @@ main() {
         exit
     fi
 
-    rm -rf .cr-release-packages
-    mkdir -p .cr-release-packages
-
-    rm -rf .cr-index
-    mkdir -p .cr-index
+    rm -rf .deploy
+    mkdir -p .deploy
 
     echo "Identifying changed charts since tag '$latest_tag'..."
 
@@ -51,6 +49,7 @@ main() {
         done
 
         release_charts
+        sleep 5
         update_index
     else
         echo "Nothing to do. No chart changes detected."
@@ -67,26 +66,42 @@ find_latest_tag() {
 
 package_chart() {
     local chart="$1"
-    helm dependency build "$chart"
-    helm package "$chart" --destination .cr-release-packages
+    Helm dependency build "$chart"
+    Helm package "$chart" --destination .deploy
 }
 
 release_charts() {
-    cr upload -o "$GIT_USERNAME" -r "$GIT_REPO"
+    chart-releaser upload -o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME" -p .deploy
 }
 
 update_index() {
-    cr index -o "$GIT_USERNAME" -r "$GIT_REPO" -c "$CR_INDEX_URL"
+    chart-releaser index -o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME" -p .deploy/index.yaml
 
     git config user.email "$GIT_EMAIL"
     git config user.name "$GIT_USERNAME"
 
+    for file in charts/*/*.md; do
+        if [[ -e $file ]]; then
+            mkdir -p ".deploy/docs/$(dirname "$file")"
+            cp --force "$file" ".deploy/docs/$(dirname "$file")"
+        fi
+    done
+
     git checkout gh-pages
-    cp --force .cr-index/index.yaml index.yaml
-    git add index.yaml
-    git commit --message="[ci skip] Update index.yaml" --signoff
-    echo "pushing to: $GIT_REPOSITORY_URL"
-    git push "$GIT_REPOSITORY_URL" gh-pages
+    cp --force .deploy/index.yaml index.yaml
+
+    if [[ -e ".deploy/docs/charts" ]]; then
+        mkdir -p charts
+        cp --force --recursive .deploy/docs/charts/* charts/
+    fi
+
+    git checkout master -- README.md
+
+    if ! git diff --quiet; then
+        git add .
+        git commit --message="Update index.yaml" --signoff
+        git push "$GIT_REPOSITORY_URL" gh-pages
+    fi
 }
 
 main
